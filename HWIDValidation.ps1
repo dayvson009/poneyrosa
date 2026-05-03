95cafc6a8d8356f559808e02321122b22842abaa
unknown <dayvson.vinicius@hotmail.com>
2026-05-03 12:33:44 -0300

    teste

--- HWIDValidation.ps1
+++ HWIDValidation.ps1
@@ -1,6 +1,4 @@
-$b64 = "MXhPdkNUcEI2OVBHWmp2UUFsQWFsNm43Sk5FQVh4TUQ3ajZ6dG84dVE2NjQ="
-$sID = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64))
-$url = "https://docs.google.com/spreadsheets/d/$sID/export?format=csv&gid=0"
+$url = "https://docs.google.com/spreadsheets/d/1xOvCTpB69PGYjvQAlAal6n7JNEAXxMD7j6zto8uQ664/export?format=csv&gid=0"
 
 function Confirm-License ($hwidLocal) {
     try {
